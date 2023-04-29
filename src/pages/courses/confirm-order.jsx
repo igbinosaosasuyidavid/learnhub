3abb1b9d2a1d1804b11fc14a474ec822a28fd8b2
Konfil Dave <igbinosasasu@gmail.com>
2023-04-29 21:32:49 +0100

    cahnged errors

--- src/pages/courses/confirm-order.jsx
+++ src/pages/courses/confirm-order.jsx
@@ -17,7 +17,7 @@ export default function ConfirmOrder() {
         console.log(cart,'------------');
         checkOrder()
        
-      }, [cart]);
+      }, [cart,checkOrder]);
     async function checkOrder() {
         const query = new URLSearchParams(window.location.search);
         if (query.get('payment')==="true") {

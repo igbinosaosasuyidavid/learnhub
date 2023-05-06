dbc5c1f776161969192dbf847f4580a9510859b6
spiffgreen <spiffjekeygreen@gmail.com>
2023-05-06 22:29:04 +0100

    feat: add wishlist feature

---
+++ src/pages/api/app/user/wishlist.js
@@ -0,0 +1,60 @@
+import prisma from "../../../../../prisma/db";
+import { authOptions } from "../../auth/[...nextauth]";
+import { getServerSession } from "next-auth";
+
+
+export default async function handler(req, res) {
+  const session = await getServerSession(req, res, authOptions);
+
+  if (!session.id) {
+    return res.status(403).json({ success: false, message: "Auth error" });
+  }
+
+  if (req.method?.toUpperCase() === "POST") {
+    // Add new course as user wishlist
+    const { courseId } = req.body;
+    const exist = await prisma.wishlist.findFirst({
+      where: {
+        courseId,
+        userId: session.id
+      }
+    });
+    if (exist) return res.status(200).json({ success: false, message: "Already in wishlist", data: exist });
+    const newWish = await prisma.wishlist.create({
+      data: {
+        courseId,
+        userId: session.id
+      }
+    });
+    return res.status(201).json({ success: true, message: "Successfully added to wishlist", data: newWish });
+  } else if (req.method?.toUpperCase() === "GET") {
+    // Return users wishlists
+    const wishes = await prisma.wishlist.findMany({
+      where: {
+        userId: session.id
+      },
+      include: {
+        course: {
+          include: {
+            author: true
+          }
+        }
+      }
+    });
+    return res.json({ success: true, message: "User wishlist", data: wishes });
+  } else if (req.method?.toUpperCase() === "PUT") {
+    // Remove from wishlist
+    const { courseId } = req.body;
+    const wish = await prisma.wishlist.findFirst({
+      where: { courseId, userId: session?.id }
+    });
+    if (wish) {
+      await prisma.wishlist.delete({
+        where: { id: wish.id }
+      });
+    }
+    return res.json({ success: true, message: "Successfully removed from wishlist" });
+  } else {
+    return res.status(400).json({ success: false, message: "Method not supported" });
+  }
+}
---
+++ src/pages/user/wishlist.jsx
@@ -0,0 +1,117 @@
+import Footer from "@/components/footer";
+import Nav from "@/components/nav";
+import axios from "axios";
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
+import { BsCart3 } from "react-icons/bs";
+import { FaUserTie } from "react-icons/fa";
+
+function Wishlist() {
+  const [wishes, setWishes] = useState([]);
+  const [courses, setCourses] = useState([]);
+
+  useEffect(() => {
+    async function getWishes() {
+      try {
+        const result = await axios.get("/api/app/user/wishlist");
+        const filter = result.data.data.map(wish => wish.course);
+        setCourses(filter);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    getWishes();
+  }, []);
+
+  return (
+    <div>
+      <Nav />
+
+      {courses.length !== 0 ? (
+        <div className="grid md:grid-cols-3 lg:grid-cols-4 gap-5 xs:grid-cols-2 mb-8">
+          {courses
+            .map((data) => {
+              return (
+                <div
+                  className="shadow-lg border border-solid border-gray-500 "
+                  key={data.id}
+                >
+                  <Image
+                    src={data.featuredImage}
+                    alt="course-Image"
+                    className="w-full object-cover sm:h-[150px] xs:h-[100px] cursor-pointer"
+                    width={300}
+                    height={300}
+                    onClick={() => {
+                      setShowLoader(true);
+                      router.push(
+                        { pathname: `/courses/${data.id}` },
+                        "/courses/" + data.id
+                      );
+                    }}
+                  />
+                  <div className="p-3">
+                    <h3
+                      className="text-black sm:text-sm xs:text-[10px] font-semibold cursor-pointer"
+                      onClick={() => {
+                        setShowLoader(true);
+                        router.push(
+                          { pathname: `/courses/${data.id}` },
+                          "/courses/" + data.id
+                        );
+                      }}
+                    >
+                      {data.title.length > 50
+                        ? data.title.slice(0, 50) + "..."
+                        : data.title}
+                    </h3>
+                    <p className="flex items-center sm:text-xs xs:text-[9px] gap-1 mb-3 mt-1">
+                      <FaUserTie className="text-secondary " />
+                      {data.author.fullName}
+                    </p>
+
+                    <h3 className="font-bold sm:text-lg xs:text-[13px] flex items-center pb-3 z-50">
+                      <span>
+                        {new Intl.NumberFormat("en-US", {
+                          style: "currency",
+                          currency: "USD",
+                          currencyDisplay: "narrowSymbol",
+                          minimumFractionDigits: 0,
+                        }).format(parseFloat(data.price).toFixed(3))}
+                      </span>
+                      <button
+                        className="flex items-center gap-1 border bg-secondary text-white hover:opacity-90 duration-300 sm:py-1 sm:px-2 xs:py-1 xs:px-1 text-[11px] rounded-md ml-auto"
+                        onClick={() => {
+                          addToCart(data, (err) => {
+                            if (err) {
+                              setToast(`Already in your cart`, "info");
+                            } else {
+                              setToast(`Added to cart.`, "success");
+                              window.scrollTo(0, 0);
+                            }
+                          });
+                        }}
+                      >
+                        {" "}
+                        <BsCart3 className="sm:text-[17px] xs:text-[11px]" />
+                      </button>
+                    </h3>
+                  </div>
+                </div>
+              );
+            })}
+        </div>
+      ) : (
+        <div className="flex items-center justify-center mb-12">
+          <p className="text-center text-sm text-slate-400">
+            You have no wishes
+          </p>
+        </div>
+      )}
+
+      <Footer />
+    </div>
+  );
+}
+
+export default Wishlist;
